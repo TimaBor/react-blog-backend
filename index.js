9acd89bd7768390af35637ea54930671087611df
TimaBor <tima-bor@yandex.ru>
2022-08-21 21:53:35 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import cors from 'cors';
 import multer from 'multer';
 import mongoose from 'mongoose';
@@ -19,6 +20,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
